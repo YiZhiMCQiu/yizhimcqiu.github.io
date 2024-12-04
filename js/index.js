27a2be32fba27ba63fc506fca12e7ce76b578387
YiZhiMCQiu <pikachu20130124@163.com>
2024-12-04 22:22:32 +0800

    添加了未完成的模组页面, 修改了部分css及js, 为body添加了测试用的100个元素

--- js/index.js
+++ js/index.js
@@ -3,3 +3,8 @@ var homeLinkElements = document.getElementsByClassName("home-link");
 for (var i = 0;i < homeLinkElements.length;i++) {
     homeLinkElements[i].setAttribute("href", window.location.origin);
 }
+for (var i = 0;i < 100;i++) {
+    var p = document.createElement("p");
+    p.textContent = i;
+    document.getElementById("body").appendChild(p);
+}
