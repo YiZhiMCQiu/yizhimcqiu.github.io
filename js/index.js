aac7f8feda1f53a1392319de3dc511c66de9c4bb
YiZhiMCQiu <pikachu20130124@163.com>
2024-12-05 08:24:19 +0800

    增加了深色模式支持, 修改了一下mods.html

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 var homeLinkElements = document.getElementsByClassName("home-link");
 
+function isDarkMode() {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
 for (var i = 0;i < homeLinkElements.length;i++) {
     homeLinkElements[i].setAttribute("href", window.location.origin);
 }
@@ -7,4 +11,11 @@ for (var i = 0;i < 100;i++) {
     var p = document.createElement("p");
     p.textContent = i;
     document.getElementById("body").appendChild(p);
+}
+
+if (isDarkMode()) {
+    document.documentElement.style.setProperty("--background-filter", "saturate(0.2) brightness(20%)")
+} else {
+    document.documentElement.style.setProperty("--background-filter", "saturate(0.2) brightness(20%) invert(100%)")
+    document.documentElement.style.setProperty("--font-color", "#051429")
 }
